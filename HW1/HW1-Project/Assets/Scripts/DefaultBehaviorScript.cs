138a52eb2ec1d71ce5cce5b9300715ea7ddc30d0
YusufBayam <yusufbayam@gmail.com>
2024-10-19 00:21:16 +0300

    Create a script and assign it to the movable cube

---
+++ HW1/HW1-Project/Assets/Scripts/DefaultBehaviorScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultBehaviorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start called!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update called!");
+    }
+}
