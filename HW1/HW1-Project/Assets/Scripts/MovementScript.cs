a3fb32e8a2168071d977bc18533decd63013638a
YusufBayam <yusufbayam@gmail.com>
2024-10-19 20:03:06 +0300

    Fix movement script when camera follows

--- HW1/HW1-Project/Assets/Scripts/MovementScript.cs
+++ HW1/HW1-Project/Assets/Scripts/MovementScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MovementScript : MonoBehaviour
 {
     public Rigidbody rb;
+    public Camera mainCamera;
     public Vector3 forcePositiveX = new Vector3(10, 0, 0);
     public Vector3 forceNegativeX = new Vector3(-10, 0, 0);
     public Vector3 forcePositiveZ = new Vector3(0, 0, 10);
@@ -28,32 +29,39 @@ public class MovementScript : MonoBehaviour
 
     void ApplyForceInDirection()
     {
+            Vector3 forward = mainCamera.transform.forward;
+            forward.y = 0; // Keep only horizontal direction
+            forward.Normalize();
+
+            Vector3 right = mainCamera.transform.right;
+            right.y = 0; // Keep only horizontal direction
+            right.Normalize();
         if (rb != null)
         {
             if (Input.GetKey(KeyCode.W))
             {
                 Debug.Log("W pressed");
-                rb.AddForce(forcePositiveZ);
+                rb.AddForce(forward * forcePositiveZ.z);
             }
-            else if (Input.GetKey(KeyCode.S))
+            if (Input.GetKey(KeyCode.S))
             {
                 Debug.Log("S pressed");
-                rb.AddForce(forceNegativeZ);
+                rb.AddForce(forward * forceNegativeZ.z);
             }
-            else if (Input.GetKey(KeyCode.A))
+            if (Input.GetKey(KeyCode.A))
             {
                 Debug.Log("A pressed");
-                rb.AddForce(forceNegativeX);
+                rb.AddForce(right * forceNegativeX.x);
             }
-            else if (Input.GetKey(KeyCode.D))
+            if (Input.GetKey(KeyCode.D))
             {
                 Debug.Log("D pressed");
-                rb.AddForce(forcePositiveX);
+                rb.AddForce(right * forcePositiveX.x);
             }
         }
         else
         {
-            Debug.Log("RigidBody is null");
+            Debug.Log("RigidBody or camera is null");
         }
     }
 }
